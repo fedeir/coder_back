162114d2d524110163ab70094aa32370dc573157
Federico Irala <=>
2021-12-13 18:51:37 -0300

    Creacion de ruta productosRandom

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const ex = require('express');
 const prod = require('./contenedor.js') ;
 
+//Numero aleatorio
+ numeroAleatorio = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+ }
+
 const ruta = './productos.txt';
 let productos = [];
 let registros = new prod.contenedor(ruta);
 
+
 registros.getAll().then(reg=>productos=reg);
 const app = ex();
-
+console.log(productos.length);
 const PORT = 8080;
 
 const server = app.listen(PORT, () => {
@@ -19,11 +25,11 @@ server.on("error", error => console.log(`error en servvidor ${error}`));
 app.get('/productos', (req, res) => {
     
     res.send(productos);
-
+    console.log(productos.length);
 })
 
-app.get('/random', (req, res) => {
+app.get('/productoRandom', (req, res) => {
     
-    res.send(productos);
-
+    numAleatorio = numeroAleatorio(0,productos.length)
+    res.send(productos[numAleatorio]);
 })
